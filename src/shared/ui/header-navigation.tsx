a73eb7c0acb6880d227edc3934b3bf8eb5415327
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2026-05-29 14:01:32 +0900

    feat: nav 컨테이너 안에 섹션 나누기

--- src/shared/ui/header-navigation.tsx
+++ src/shared/ui/header-navigation.tsx
@@ -1,8 +1,10 @@
 export default function NavigaitionHeader() {
   return (
     <>
-    <div id="headerContainer">
-
+    <div id="headerContainer" className="fixed top-0 left-0 w-full h-20 bg-white px-32 flex items-center justify-center">
+      <div id="logo"></div>
+      <div id="navMenu"></div>
+      <div id="userMenu"></div>
     </div>
     </>
   );
