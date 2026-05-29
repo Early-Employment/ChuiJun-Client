89e58d187217f1a96fb8aea6425f74f794485b66
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2026-05-29 14:58:42 +0900

    feat:logo 및 link 추가

--- src/shared/ui/header-navigation.tsx
+++ src/shared/ui/header-navigation.tsx
@@ -1,11 +1,21 @@
+import Link from "next/link";
+import { LogoIcon } from "@/shared/assets/LogoIcon";
+
 export default function NavigaitionHeader() {
   return (
     <>
-    <div id="headerContainer" className="fixed top-0 left-0 w-full h-20 bg-white px-32 flex items-center justify-center">
-      <div id="logo"></div>
-      <div id="navMenu"></div>
-      <div id="userMenu"></div>
-    </div>
+      <div id="headerContainer" className="fixed top-0 left-0 flex h-20 w-full items-center justify-center bg-white px-32">
+        <div id="logo" className="flex items-center gap-2">
+          <LogoIcon className="h-8 w-auto" />
+          <p>취준</p>
+        </div>
+        <div id="navMenu" className="flex">
+          <Link href="/main">홈</Link>
+          <Link href="/ranking">랭킹</Link>
+          <Link href="/class">학급</Link>
+        </div>
+        <div id="userMenu"></div>
+      </div>
     </>
   );
 }
