f87c6fbd9670815cb6ec8dc1548482675b3e90d0
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2026-05-29 19:45:36 +0900

    feat:main 레이아웃 잡기

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,7 +1,11 @@
 export default function MainPage() {
   return (
-    <div id="container" className="flex h-screen">
-      <div id="header" className="flex w-full h-80 bg-primary-500"></div>
+    <div id="container" className="flex flex-col h-screen">
+      <div id="header" className="flex w-full min-h-80 bg-primary-500"></div>
+      <div id="content" className="flex w-full h-auto bg-[#E9EBF1] px-35 py-8">
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
